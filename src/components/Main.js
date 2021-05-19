8779ff6c9456c7fa86374baaf1a4ff9a0cd8df22
AKatryukhin <A.Katryukhin@yandex.ru>
2021-05-19 14:53:12 +0300

    style: replace Main, Header, Footer from landing to components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Card from '../Card.js';
-import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+import Card from './Card.js';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Main({
   onEditAvatar,
